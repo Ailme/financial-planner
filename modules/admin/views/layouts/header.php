644c3e682a4be9cf0ebfd5974b10c6006548db79
Tumaykin Alexandr <alexandrtumaykin@gmail.com>
2017-09-02 13:45:54 +0300

    fix menu

--- modules/admin/views/layouts/header.php
+++ modules/admin/views/layouts/header.php
@@ -1,4 +1,5 @@
 <?php
+
 use yii\helpers\Html;
 
 /* @var $this \yii\web\View */
@@ -7,7 +8,8 @@ use yii\helpers\Html;
 
 <header class="main-header">
 
-    <?= Html::a('<span class="logo-mini">APP</span><span class="logo-lg">' . Yii::$app->name . '</span>', Yii::$app->homeUrl, ['class' => 'logo']) ?>
+    <?= Html::a('<span class="logo-mini">APP</span><span class="logo-lg">' . Yii::$app->name . '</span>',
+        Yii::$app->homeUrl, ['class' => 'logo']) ?>
 
     <nav class="navbar navbar-static-top" role="navigation">
 
@@ -229,51 +231,27 @@ use yii\helpers\Html;
 
                 <li class="dropdown user user-menu">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                        <img src="<?= $directoryAsset ?>/img/user2-160x160.jpg" class="user-image" alt="User Image"/>
-                        <span class="hidden-xs">Alexander Pierce</span>
+                        <span class="fa fa-user fa-2x user-image"></span>
+                        <span class="hidden-xs"><?= Yii::$app->user->getIdentity()->username ?></span>
                     </a>
-                    <ul class="dropdown-menu">
-                        <!-- User image -->
-                        <li class="user-header">
-                            <img src="<?= $directoryAsset ?>/img/user2-160x160.jpg" class="img-circle"
-                                 alt="User Image"/>
 
-                            <p>
-                                Alexander Pierce - Web Developer
-                                <small>Member since Nov. 2012</small>
-                            </p>
-                        </li>
-                        <!-- Menu Body -->
-                        <li class="user-body">
-                            <div class="col-xs-4 text-center">
-                                <a href="#">Followers</a>
-                            </div>
-                            <div class="col-xs-4 text-center">
-                                <a href="#">Sales</a>
-                            </div>
-                            <div class="col-xs-4 text-center">
-                                <a href="#">Friends</a>
-                            </div>
-                        </li>
-                        <!-- Menu Footer-->
-                        <li class="user-footer">
-                            <div class="pull-left">
-                                <a href="#" class="btn btn-default btn-flat">Profile</a>
-                            </div>
-                            <div class="pull-right">
-                                <?= Html::a(
-                                    'Sign out',
-                                    ['/site/logout'],
-                                    ['data-method' => 'post', 'class' => 'btn btn-default btn-flat']
-                                ) ?>
-                            </div>
-                        </li>
-                    </ul>
-                </li>
-
-                <!-- User Account: style can be found in dropdown.less -->
-                <li>
-                    <a href="#" data-toggle="control-sidebar"><i class="fa fa-gears"></i></a>
+                    <?= dmstr\widgets\Menu::widget(
+                        [
+                            'defaultIconHtml' => '',
+                            'options' => ['class' => 'dropdown-menu'],
+                            'items' => [
+                                [
+                                    'label' => Yii::t('app', 'NAV_PROFILE'),
+                                    'url' => ['/user/profile/index'],
+                                ],
+                                [
+                                    'label' => Yii::t('app', 'NAV_LOGOUT'),
+                                    'url' => ['/user/default/logout'],
+                                    'linkOptions' => ['data-method' => 'post'],
+                                ],
+                            ],
+                        ]
+                    ) ?>
                 </li>
             </ul>
         </div>
